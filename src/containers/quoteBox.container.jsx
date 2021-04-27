bce86d9f135196a7187a56ddf11fa2e9bb715a74
vinjas <danielvinyastorres@gmail.com>
2021-04-27 13:20:51 +0200

    Update quote component and change jsx for container

---
+++ src/containers/quoteBox.container.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import QuoteBox from "../components/quoteBox.component"
+
+const QuoteContainer =
+    <div>
+        <QuoteBox/>
+    </div>
+
+
+export default QuoteContainer
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from "react"
 import ReactDOM from "react-dom"
-import QuoteContainer from "./containers/quoteBox.container.js"
+import QuoteContainer from "./containers/quoteBox.container.jsx"
 
 
-
-ReactDOM.render(<QuoteContainer />, document.getElementById("root"))
+ReactDOM.render(QuoteContainer, document.getElementById("root"))
